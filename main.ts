de5adb7b5c663a5019ce1bd8da0a5cb7ae4138e4
David May <David.May@efi.com>
2022-04-04 23:36:49 -0700

    Fixed rendering issues by using onNodeInserted() since Raphael requires div already part of DOM. Extended error dialog w/ examples and issues links.

--- main.ts
+++ main.ts
@@ -1,76 +1,80 @@
 import { App, MarkdownRenderChild, Notice, Plugin, PluginSettingTab } from 'obsidian';
-import {randomUUID} from "crypto";
 const jtab = require("jtab");
 const OBSIDIAN_JTAB_CLASSES: {[jtype:string]: string} = {'jtab': "jtab", 'jtab-chordonly': "jtab chordonly", 'jtab-tabonly': "jtab tabsonly"};
 const OBSIDIAN_JTAB_TYPES = Object.keys(OBSIDIAN_JTAB_CLASSES);
 
-
-class ObsidianJTabBlock extends MarkdownRenderChild {
-	renderId: string;
+class ObsidianJTabCodeBlock extends MarkdownRenderChild {
+	private _jTabDestroyListener: () => void = null;
+	private _jTabDiv: HTMLElement = null;
 	constructor(
-		public plugin: ObsidianJTab,
+		public plugin: ObsidianJTabPlugin,
 		public src: string,
 		public containerEl: HTMLElement,
 		public jtype: string
 	) {
 		super(containerEl);
-		if (!OBSIDIAN_JTAB_TYPES.includes(jtype)) {
-			throw new Error(`ObsidianJTabBlock: Unknown jTab type '${jtype}'`);
-		}
-		this.plugin = plugin;
-		this.src = src;
-		this.jtype = jtype;
-		this.renderId = `jtab-${randomUUID()}`
-	}
-
-	onload(): void {
-		this.renderJTab();
-	}
-
-	async renderJTab() {
-
-		if (this.src.trim().length) {
-			try {
-				const jSrc = `<div class="${OBSIDIAN_JTAB_CLASSES[this.jtype]}">${this.src}</div>`;
-				const jdiv = this.containerEl.createDiv({attr: {'id': this.renderId}});
-				debugger;
-				jtab.render(jdiv, "A B C" ); // jSrc);
-			} catch (e) {
-				this.renderJTabError(e);
-			}
+		if (!OBSIDIAN_JTAB_TYPES.includes(this.jtype)) {
+			throw new Error(`Unknown jTab type '${this.jtype}'`);
 		}
 	}
 
-	renderJTabError(e: any) {
-		const ediv = this.containerEl.createDiv({cls: 'jtab-error'});
-		ediv.createDiv({cls: 'jtab-error-title', text: 'Error: Failed to generated jTab.'});
-		ediv.createDiv({cls: 'jtab-error-message', text: `=> ${e.message}`});
-		ediv.createEl('pre').createEl('code', {cls: 'jtab-error-src', text: `\`\`\`${this.jtype}\n${this.src}\n\`\`\``});
-	}
-}
-
-export default class ObsidianJTab extends Plugin {
 	async onload() {
-		// This is just an informational page, no settings are loaded/saved
-		this.addSettingTab(new ObsidianJTabSettingsTab(this.app, this));
-
-		for (let jtype of OBSIDIAN_JTAB_TYPES) {
-			this.registerMarkdownCodeBlockProcessor(jtype, (src, el, ctx) => {
-				const handler = new ObsidianJTabBlock(this, src, el, jtype);
-				ctx.addChild(handler);
-			});	
+		const srcLines = this.src.split('\n').filter(e => e.trim().length);
+		if (srcLines.length) {
+			this._jTabDiv = this.containerEl.createDiv();
+			// Raphael will only render SVGs on existing DOM elements
+			this._jTabDestroyListener = this._jTabDiv.onNodeInserted(() => {
+				try {
+					srcLines.forEach((srcLine) => {
+						const tgtDiv = this._jTabDiv.createDiv({
+							cls: OBSIDIAN_JTAB_CLASSES[this.jtype]
+						});
+			
+						jtab.render(tgtDiv, srcLine);
+						if (!tgtDiv.classList.contains("rendered")) {
+							throw new Error("Invalid jTab syntax");
+						}	
+					});
+				} catch (e) {
+					this._cleanup();
+					this._displayJTabError(e);
+				}
+			}, true);
 		}
 	}
 
-	onunload() {
+	async onunload() {
+		this._cleanup();
+	}
 
+	private _cleanup() {
+		if (this._jTabDiv) {
+			this._jTabDiv.detach();
+			this._jTabDiv = null;
+		}
+		if (this._jTabDestroyListener) {
+			this._jTabDestroyListener();
+			this._jTabDestroyListener = null;
+		}
+	}
+
+	private _displayJTabError(e: any): void {
+		this._jTabDiv = this.containerEl.createDiv({cls: 'jtab-error'});
+		this._jTabDiv.createDiv({cls: 'jtab-error-title', text: 'Error: Failed to generated jTab'});
+		this._jTabDiv.createDiv({cls: 'jtab-error-message', text: `=> ${e.message}`});			
+		this._jTabDiv.createEl('pre').createEl('code', {cls: 'jtab-error-src', text: `\`\`\`${this.jtype}\n${this.src}\n\`\`\``});
+		const help = this._jTabDiv.createSpan({cls: 'jtab-error-help'})
+		help.createEl('a', {href: 'https://jtab.tardate.com/examples.htm', text: 'Examples'});
+		help.createSpan({text: " • "});
+		help.createEl('a', {href: 'https://github.com/davfive/obsidian-jtab/issues', text: 'Issues'});
 	}
 }
+
 class ObsidianJTabSettingsTab extends PluginSettingTab {
 	// This is just an informational page, no settings are loaded/saved
-	plugin: ObsidianJTab;
+	plugin: ObsidianJTabPlugin;
 
-	constructor(app: App, plugin: ObsidianJTab) {
+	constructor(app: App, plugin: ObsidianJTabPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -82,7 +86,7 @@ class ObsidianJTabSettingsTab extends PluginSettingTab {
 
 		containerEl.createEl('h2', {text: 'Obsidian jTab'});
 		let p = containerEl.createEl('p');
-		p.createEl('span', {text: "The Obsidian jTab plugin is not affiliated in any way with jTab. It only adds jtab blocks to Obsidian."});
+		p.createEl('p', {text: "The Obsidian jTab plugin is not affiliated in any way with jTab. It only adds jtab blocks to Obsidian."});
 
 		p = containerEl.createEl('p');
 		p.createEl('span', {text: "Visit jTab's home page for "});
@@ -95,5 +99,23 @@ class ObsidianJTabSettingsTab extends PluginSettingTab {
 		p.createEl('span', {text: "This plugin's source code can be found on "});
 		p.createEl('a', {href: 'https://github.com/davfive/obsidian-jtab', text: 'GitHub'});
 		p.createEl('span', {text: '.'});
+	}
+}
+
+export default class ObsidianJTabPlugin extends Plugin {
+	async onload() {
+		// This is just an informational page, no settings are loaded/saved
+		this.addSettingTab(new ObsidianJTabSettingsTab(this.app, this));
+
+		for (let jtype of OBSIDIAN_JTAB_TYPES) {
+			this.registerMarkdownCodeBlockProcessor(jtype, (src, el, ctx) => {
+				const handler = new ObsidianJTabCodeBlock(this, src, el, jtype);
+				ctx.addChild(handler);
+			});	
+		}
+	}
+
+	async onunload() {
+		// No cleanup required at the plugin level
 	}
 }
